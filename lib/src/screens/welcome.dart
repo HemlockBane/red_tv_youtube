b163eaa1f1479b52aded1375d0ed34e22b9cb15f
Obinna Igwe <igwenus619@gmail.com>
2020-07-29 13:34:47 +0100

    Add placeholder text

--- lib/src/screens/welcome.dart
+++ lib/src/screens/welcome.dart
@@ -13,12 +13,30 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff3F3F3F),
       body: SingleChildScrollView(
         child: Container(
           margin: EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             children: <Widget>[
+              SizedBox(
+                height: 55,
+              ),
+
               /// ..Add your code here
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Welcome to',
+                    style: TextStyle(
+                      color: Color(0xFFCACACA),
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
               _buildPopularNowSection(),
               _buildExclusivesSection()
             ],
