ef49a6bec1009e4db00ec2797604a8c8c1e72ea2
chukschiboy <chukschiboy@gmail.com>
2020-08-03 00:07:47 +0100

    update on the login screen

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -10,24 +10,42 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          color: Colors.red,
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) {
-                return WelcomeScreen(
-                  title: 'Red TV Youtube',
-                );
-              }),
-            );
-          },
-          child: Text(
-            'Login',
-            style: TextStyle(color: Colors.white),
+      backgroundColor: Color(0xff3F3F3F),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 100,
+            height: 100,
+            child: Image.asset('assets/images/red_tv.jpg'),
           ),
-        ),
+          SizedBox(height: 15),
+          Center(
+            child: MaterialButton(
+              padding: EdgeInsets.symmetric(vertical: 7, horizontal: 30),
+              color: Colors.red[800],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(3)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return WelcomeScreen(
+                      title: 'Red TV Youtube',
+                    );
+                  }),
+                );
+              },
+              child: Text(
+                'Login'.toUpperCase(),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
